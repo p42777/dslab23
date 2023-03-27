cc069616c543cb8646d8652b6ded0100ff08a776
Clyde Lu <92775935+p42777@users.noreply.github.com>
2023-03-28 01:00:06 +0800

    0328

--- 03_remoting/soap/soapClient.js
+++ 03_remoting/soap/soapClient.js
@@ -1,6 +1,6 @@
 const soap = require('soap');
-const url = 'http://localhost:8192/Adder?wsdl';
-var args = {x: 3, y: 2};
+const url = 'http://172.17.0.2:8192/AddMul?wsdl';
+var args = {x: 10, y: 20};
 
 soap.createClient(url, function (err, client) {
     if (err) console.log(err);
@@ -10,5 +10,13 @@ soap.createClient(url, function (err, client) {
         console.log(rawRequest);
         console.log('');
         console.log(rawResponse);
+        console.log("-----------------divide--------------------");
+    });
+    client.multiply(args, function (err, result,  rawResponse, soapHeader, rawRequest) {
+        if (err) console.log(err);
+        console.log(rawRequest);
+        console.log('');
+        console.log(rawResponse);
+        console.log("-----------------divide--------------------");
     });
 });
--- 03_remoting/soap/soapServer.js
+++ 03_remoting/soap/soapServer.js
@@ -1,11 +1,14 @@
 const soap = require('soap');
 const http = require('http');
-const xml = require('fs').readFileSync('Adder.wsdl', 'utf8');
+const xml = require('fs').readFileSync('AddMul.wsdl', 'utf8');
 const service = {
     CalculatorImplService: {
         CalculatorImplPort: {
             add: function (args) {
                 return {result: args.x + args.y};
+            },
+            multiply: function (args) {
+                return {result: args.x * args.y};
             }
         }
     }
@@ -15,7 +18,7 @@ const server = http.createServer(function (request, response) {
     response.end('404: Not Found: ' + request.url);
 });
 
-soap.listen(server, '/Adder', service, xml, function () {
+soap.listen(server, '/AddMul', service, xml, function () {
     console.log('server initialized');
 });
 
